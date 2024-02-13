15a57d54d9b2e75edc8b3be1e1f3da3cbd9332eb
Danial Shah <gillanid11@gmail.com>
2024-02-13 16:55:00 +0500

    Updates

---
+++ static/js/193.b60337fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcapstone=self.webpackChunkcapstone||[]).push([[193],{193:(e,n,s)=>{s.r(n),s.d(n,{default:()=>a});var c=s(184);const a=()=>(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("section",{className:"container",children:[(0,c.jsx)("h1",{children:"404"}),(0,c.jsx)("p",{children:"We can't find what you are looking for"})]})})}}]);
+//# sourceMappingURL=193.b60337fd.chunk.js.map
